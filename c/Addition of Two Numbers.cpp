c3548395ac948dd088239fd50e8c479fdf261049
Subiksha <96659807+subi03@users.noreply.github.com>
2024-02-10 20:34:42 +0530

    Create Addition of Two Numbers.cpp

---
+++ c/Addition of Two Numbers.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+  //Declaring three Variables
+     int x, y, sum;           
+
+ //Input  Numbers
+   printf("Enter Two Numbers : \n");    
+   scanf("%d %d",&x, &y);     
+
+  //Calculating Sum of Two numbers
+   sum = x + y ;
+
+//Desplaying Sum
+   printf("Sum is : %d", sum);    
+
+}
